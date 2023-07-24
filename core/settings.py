a6cec720bb767933604d3e39cb37731636c4a6fe
Dario-Yoshida <darioyoshida@hotmail.com>
2023-07-23 21:26:49 -0300

    mudança requirements django

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,6 +96,12 @@ DATABASES = {
         
     }
 }
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    }
 
 
 # Password validation
